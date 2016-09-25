edd14258858fbb4d4a7cfd5617bd15cffad9e675
Syed Aman <sna274@nyu.edu>
2016-09-25 10:31:36 -0400

    create empty React components for blueprint

---
+++ public/scripts/script.js
@@ -0,0 +1,21 @@
+// component structure:
+// - CommentBox
+//     - CommentList
+//         - Comment
+//     - CommentForm
+
+// create CommentBox component
+var CommentBox = React.createClass({
+});
+
+// create CommentList component
+var CommentList = React.createClass({
+});
+
+// create Comment component
+var Comment = React.createClass({
+});
+
+// create CommentForm component
+var CommentForm = React.createClass({
+});
