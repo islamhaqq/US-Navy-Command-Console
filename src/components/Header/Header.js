4a2723cc469d112dc5c077330c37071c84545577
Syed Aman <sna274@nyu.edu>
2017-01-10 18:19:07 -0500

    change company name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ class Header extends React.Component {
           <Navigation className={s.nav} />
           <Link className={s.brand} to="/">
             <img src={logoUrl} srcSet={`${logoUrl2x} 2x`} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
+            <span className={s.brandTxt}>US Navy</span>
           </Link>
           <div className={s.banner}>
             <h1 className={s.bannerTitle}>React</h1>
