3f5bbefa6f7d1f2d7141ab2674c8ea01b55445d8
Syed Aman <sna274@nyu.edu>
2017-01-15 08:14:52 -0500

    loop through SampleTrackData for vessel _id

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -3,13 +3,44 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 import IconButton from 'material-ui/IconButton';
-import {red500, yellow500, blue500} from 'material-ui/styles/colors';
+import {blue500} from 'material-ui/styles/colors';
+
+// import Sample Track Data
+import SampleTrackData from '../../data/SampleTrackData.json';
 
 export default class DrawerMenu extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {open: false};
+
+    // loop through SampleTrackData
+    function PopulateTrackList() {
+      for (var vessel in SampleTrackData.tracks) {
+        // filter unwanted properties
+        if (SampleTrackData.tracks.hasOwnProperty(vessel)) {
+          var id = SampleTrackData.tracks[vessel]._id;
+          console.log(id);
+        }
+      }
+    }
+    PopulateTrackList();
+
+    // function PopulateTrackList(SampleTrackData) {
+    //   return SampleTrackData.map((article, index) => (
+    //       <Article key={index} article={article} />
+    //   ));
+    // }
+    //
+    // const Article = ({article}) => {
+    //     return (
+    //         <article key={article.id}>
+    //             <a href={article.link}>{article.title}</a>
+    //             <p>{article.description}</p>
+    //         </article>
+    //     );
+    // };
+
   }
 
   // handle drawer state
